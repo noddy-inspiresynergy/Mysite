d8e47cde58d1a4032e2f2453ad212312a4ec642d
Noddy Ng <noddy.ng@inspiresynergy.com>
2011-11-28 17:47:29 +0800

    Mysite Git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mysite::Application.config.secret_token = '449df0e22040f429b1ea484e3eac5af9e1bda36015c7a5fe8e7020fd88dc2966876998ff467d735352e95d770e443a866722d9f1879372b38cfd5526bee2d62a'
---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+
+Product.create([{:title => 'Best Product Ever', :description => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', :image_url => 'images/book01.jpg', :price => '99.9'}])
